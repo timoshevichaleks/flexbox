d640064d063dbb612784501feba2d07bc7deab08
timoshevichaleks <alti2692@gmail.com>
2021-05-24 14:46:56 +0300

    исправил шрифт, интервалы и кнопку

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,20 @@
 (function($) {
 
-  $(document).ready(function() {
-    $('body').niceScroll();
-  });
+  // $(document).ready(function() {
+  //   $('body').niceScroll();
+  // });
 
-  $('.header-menu__btn').click(function() {
+  $('.header-menu__btn').on('click', function() {
     $(this).toggleClass('active');
-    $('.header-menu__list').toggleClass('active');
+    $('.header-menu__list').stop(true, true).slideToggle(500);
+    // $('.header-menu__list').toggleClass('active');
   });
-
-
 
 })(jQuery);
 
-$(document).ready(function() {
-  $('.header-menu-btn').click(function() {
-    $(this).toggleClass('open');
-    $('.header-menu__list').stop(true, true).slideToggle(500)
-  });
-});
+// $(document).ready(function() {
+//   $('.header-menu__btn').click(function() {
+//     $(this).toggleClass('open');
+//     $('.header-menu__list').stop(true, true).slideToggle(500);
+//   });
+// });
